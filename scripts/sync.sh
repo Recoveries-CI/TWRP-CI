4df1f819af2ea1c625f31cc785fe7139df248503
Luciogi <84625092+Luciogi@users.noreply.github.com>
2022-10-02 10:30:28 +0500

    Remove telegram

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -7,12 +7,12 @@ source $CONFIG
 cd ~
 
 # A Function to Send Posts to Telegram
-telegram_message() {
-	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
-	-d chat_id="${TG_CHAT_ID}" \
-	-d parse_mode="HTML" \
-	-d text="$1"
-}
+#telegram_message() {
+#	curl -s -X POST "https://api.telegram.org/bot${TG_TOKEN}/sendMessage" \
+#	-d chat_id="${TG_CHAT_ID}" \
+#	-d parse_mode="HTML" \
+#	-d text="$1"
+#}
 
 # Clone the Sync Repo
 cd $SYNC_PATH
