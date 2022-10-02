81ccc5f63de8379bbee9eb60c751750810835794
Luciogi <84625092+Luciogi@users.noreply.github.com>
2022-10-02 10:53:52 +0500

    chime twrp build A12.1

--- config.sh
+++ config.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Device
-export TWRP_MANIFEST="https://github.com/faoliveira78/platform_manifest_twrp_aosp"
+export TWRP_MANIFEST="https://github.com/minimal-manifest-twrp/platform_manifest_twrp_aosp"
 export TWRP_BRANCH="twrp-12.1"
 #export TWRP_BRANCH="twrp-test"
-export DT_LINK="https://github.com/TeamWin/android_device_oneplus_cheeseburger_dumpling.git -b android-12.1"
+export DT_LINK="https://github.com/frstprjkt/device_xiaomi_chime-recovery -b twrp-12.1"
 
-export DEVICE="cheeseburger_dumpling"
-export OEM="oneplus"
+export DEVICE="chime"
+export OEM="xiaomi"
 #export PLATFORM="msm8998"
 export TARGET="recoveryimage"
 
