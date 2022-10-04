d6f4f8775d2cd4837bab5292475721ff5d42d777
Fernando Oliveira <fernandoaju78@gmail.com>
2022-10-04 19:59:47 -0300

    new try with A13 LOS Kernel
    Signed-off-by: Fernando Oliveira <fernandoaju78@gmail.com>

--- config.sh
+++ config.sh
@@ -4,7 +4,7 @@
 export TWRP_MANIFEST="https://github.com/faoliveira78/platform_manifest_twrp_aosp"
 export TWRP_BRANCH="twrp-12.1"
 #export TWRP_BRANCH="twrp-test"
-export DT_LINK="https://github.com/TeamWin/android_device_oneplus_cheeseburger_dumpling.git -b android-12.1"
+export DT_LINK="https://github.com/faoliveira78/android_device_oneplus_cheeseburger_dumpling.git -b android-12.1"
 
 export DEVICE="cheeseburger_dumpling"
 export OEM="oneplus"
